80422007d1901c13cb7adbac7b6e47b4d20fddaa
Shad Beard <shad@wyzerr.com>
2016-04-06 15:07:43 -0400

    Add fs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var https = require('https');
+var fs = require('fs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
